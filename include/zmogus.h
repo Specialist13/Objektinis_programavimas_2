9f3f9998e15efa73aa335a29374c31d9aaded85c
Specialistas13 <emilis.slabada@gmail.com>
2025-04-25 12:46:15 +0300

    Sukurta zmogaus klase

---
+++ include/zmogus.h
@@ -0,0 +1,39 @@
+#ifndef ZMOGUS_H
+#define ZMOGUS_H
+
+#include "headers_main.h"
+
+class Zmogus {
+protected:
+    string vardas;
+    string pavarde;
+public:
+    Zmogus(string v, string p) : vardas(v), pavarde(p) {}
+    Zmogus() : vardas(""), pavarde("") {}
+
+    Zmogus(const Zmogus &z) : vardas(z.vardas), pavarde(z.pavarde) {}
+    Zmogus& operator=(const Zmogus &z) {
+        if (this != &z) {
+            vardas = z.vardas;
+            pavarde = z.pavarde;
+        }
+        return *this;
+    }
+    Zmogus(Zmogus &&z) noexcept : vardas(std::move(z.vardas)), pavarde(std::move(z.pavarde)) {}
+    Zmogus& operator=(Zmogus &&z) noexcept {
+        if (this != &z) {
+            vardas = std::move(z.vardas);
+            pavarde = std::move(z.pavarde);
+        }
+        return *this;
+    }
+
+    ~Zmogus() {
+        vardas.clear();
+        pavarde.clear();
+    }
+    virtual string getVardas() const = 0;
+    virtual string getPavarde() const = 0;
+};
+
+#endif
